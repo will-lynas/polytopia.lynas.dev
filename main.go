98f5d3c705604a2f46f0e141396b95cbc4721b20
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-01-12 16:16:17 +0000

    Add htmx POC

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
     "log"
     "net/http"
+    "time"
+    "fmt"
 )
 
 func main() {
-     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.Redirect(w, r, "/construction.jpg", http.StatusFound)
+    http.HandleFunc("/current_time", func(w http.ResponseWriter, r *http.Request) {
+        updatedContent := "Updated content at " + time.Now().Format(time.RFC1123)
+        fmt.Fprint(w, updatedContent)
     })
 
     fs := http.FileServer(http.Dir("static"))
-    http.Handle("/construction.jpg", fs)
+    http.Handle("/", fs)
 
     log.Println("Listening ...")
     err := http.ListenAndServeTLS(":8443", "certs/lynas_dev.crt", "certs/lynas_dev.key", nil)
