23aa362e530f359e1d7ae54172e3a3e5c9028f2f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-02-07 14:29:00 +0000

    Restructure project and remove htmx test (#2)

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import (
     "log"
     "net/http"
-    "time"
-    "fmt"
-    "math/rand"
-    "strconv"
 )
 
 func main() {
-    registerHTMXTestHandlers()
-    registerPolytopiaHandlers()
+    staticFiles := http.FileServer(http.Dir("static"))
+    http.Handle("/", staticFiles)
 
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/", fs)
+    http.HandleFunc("/polytopia", polytopiaHandler)
+    http.HandleFunc("/polytopia.html", polytopiaHandler)
 
     log.Println("Listening ...")
     err := http.ListenAndServeTLS(":8443",
@@ -23,81 +19,4 @@ func main() {
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
-}
-
-func registerHTMXTestHandlers() {
-    http.HandleFunc("/current_time", func(w http.ResponseWriter, r *http.Request) {
-        updatedContent := "Updated content at " + time.Now().Format(time.RFC1123)
-        fmt.Fprint(w, updatedContent)
-    })
-}
-
-func registerPolytopiaHandlers() {
-    getRandomListElement := func(list []string) string {
-        return list[rand.Intn(len(list))]
-    }
-
-    const totalPlayers = 9
-
-    http.HandleFunc("/generate-game",
-    func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPost {
-            http.Error(w, "Method is not supported.", http.StatusNotFound)
-            return
-        }
-        if err := r.ParseForm(); err != nil {
-            http.Error(w, "Error parsing form", http.StatusInternalServerError)
-            return
-        }
-        numPlayers, err := strconv.Atoi(r.FormValue("num-players"))
-        if err != nil {
-            http.Error(w, "Error parsing number of players",
-            http.StatusInternalServerError)
-            return
-        }
-        numBots := rand.Intn(totalPlayers - numPlayers + 1)
-        mapTypes := r.Form["map-types"]
-        if len(mapTypes) == 0 {
-            http.Error(w, "No map types selected", http.StatusInternalServerError)
-            return
-        }
-        mapType := getRandomListElement(mapTypes)
-        mapSizes := r.Form["map-sizes"]
-        if len(mapSizes) == 0 {
-            http.Error(w, "No map sizes selected", http.StatusInternalServerError)
-            return
-        }
-        mapSize := getRandomListElement(mapSizes)
-        tribes := r.Form["tribes"]
-        if len(tribes) == 0 {
-            http.Error(w, "No tribes selected", http.StatusInternalServerError)
-            return
-        }
-        botDifficulties := r.Form["bot-difficulties"]
-        if len(botDifficulties) == 0 {
-            http.Error(w, "No bot difficulty selected", http.StatusInternalServerError)
-            return
-        }
-
-        responseContent := fmt.Sprintf(
-            `Map type: %s<br>
-            Map size: %s`,
-            mapType, mapSize)
-
-        responseContent += "<br><br>Bots: "
-        for i := 0; i < numBots; i++ {
-            responseContent += fmt.Sprintf(" %s", getRandomListElement(botDifficulties))
-        }
-
-        responseContent += "<br><br>Tribes:<br>"
-        for i := 0; i < numPlayers; i++ {
-            tribe := getRandomListElement(tribes)
-            responseContent += fmt.Sprintf(
-            `Player %d: <span class="spoiler">%-16s</span><br>`,
-            i+1, tribe)
-        }
-
-        w.Header().Set("Content-Type", "text/html")
-        fmt.Fprint(w, responseContent)
-    })
 }
